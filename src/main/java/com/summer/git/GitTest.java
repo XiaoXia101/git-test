18d8252ad9b967534313c87427238f7d13ece5ec
Summer <Summer@xia.com>
2023-06-14 21:12:07 +0800

    third idea commit

--- src/main/java/com/summer/git/GitTest.java
+++ src/main/java/com/summer/git/GitTest.java
@@ -7,6 +7,7 @@ package com.summer.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Summer");
+        System.out.println("Hello Summer!");
+        System.out.println("Hello World!");
     }
 }
