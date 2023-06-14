8a058cf29069220169cb79bc6dda5db677b5d7ef
Summer <Summer@xia.com>
2023-06-14 22:05:40 +0800

    push test

--- src/main/java/com/summer/git/GitTest.java
+++ src/main/java/com/summer/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello World!");
         System.out.println("........");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
