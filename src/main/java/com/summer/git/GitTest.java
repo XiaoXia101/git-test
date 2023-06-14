dea132049c77d55e0e0fd5898bc99322319bb0f1
Summer <Summer@xia.com>
2023-06-14 21:08:39 +0800

    first idea commit

---
+++ src/main/java/com/summer/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.summer.git;
+
+/**
+ * @author summer
+ * @date 2023/6/14 - 21:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
